f2a15e437616a65b337d97b5dce3ed6507a80f35
JulienMirval <julien.mirval@cozycloud.cc>
2021-11-17 18:15:41 +0100

    style: Cleaner test

--- scripts/splitClasses.test.js
+++ scripts/splitClasses.test.js
@@ -1,13 +1,9 @@
 const splitClasses = require('./splitClasses').splitClasses
 
 test('Returns a list of list of classes', () => {
-  const oldLog = console.log
-  console.log = jest.fn()
+  jest.spyOn(console, 'log').mockImplementation(() => jest.fn())
 
   splitClasses(4, 2)
 
-  // The first argument of the first call to the function was 'hello'
-  expect(console.log.mock.calls[0][0]).toBe(
-    '0,100 200,300 200000,200100 200110,200120'
-  )
+  expect(console.log).toHaveBeenCalledWith('0,100 200,300 200000,200100 200110,200120')
 })
