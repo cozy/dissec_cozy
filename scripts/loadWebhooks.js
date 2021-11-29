caccbcd910b0cfeba2217089c31891ae6bdf32d2
JulienMirval <julien.mirval@cozycloud.cc>
2021-11-29 13:38:13 +0100

    feat: simplified documents interactions

--- scripts/loadWebhooks.js
+++ scripts/loadWebhooks.js
@@ -2,9 +2,8 @@ global.fetch = require('node-fetch').default
 const fs = require('fs')
 const { default: CozyClient, Q } = require('cozy-client')
 
+const { NODES_DOCTYPE } = require('../src/doctypes/nodes')
 const newWebhooks = require('../assets/webhooks.json')
-
-const NODES_DOCTYPE = 'io.cozy.dissec.nodes'
 
 const main = async () => {
   // Connect to the instance
@@ -21,15 +20,13 @@ const main = async () => {
   })
 
   // Fetch old nodes
-  const { data: oldNodes } = await client.collection(NODES_DOCTYPE).all()
+  const { data: oldNodes } = await client.queryAll(Q(NODES_DOCTYPE))
 
   // Delete them
   await client.collection(NODES_DOCTYPE).destroyAll(oldNodes)
 
   // Create a new doc for each instance
-  for(const webhook of newWebhooks) {
-    await client.collection(NODES_DOCTYPE).create(webhook)
-  }
+  await client.collection(NODES_DOCTYPE).saveAll(newWebhooks)
 }
 
 main()
