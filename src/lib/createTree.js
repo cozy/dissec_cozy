5d368509c2531d9429a5a3698c8ac5bf76425412
JulienMirval <julien.mirval@cozycloud.cc>
2023-04-05 14:13:03 +0200

    fix: Integration test

--- src/lib/createTree.js
+++ src/lib/createTree.js
@@ -12,7 +12,8 @@ const createTree = (treeStructure, nodesWebhooks) => {
   let matchingNodes = remainingNodes.slice()
   let lastLevel = []
 
-  treeStructure.forEach((level, j) => {
+  for (let j = 0; j < treeStructure.length; j++) {
+    const level = treeStructure[j]
     const currentLevel = []
     matchingNodes = remainingNodes.slice()
 
@@ -30,23 +31,46 @@ const createTree = (treeStructure, nodesWebhooks) => {
     }
 
     // Move nodes to the current level
-    for (let i = 0; i < level.numberOfNodes; i++) {
-      currentLevel.push({
-        ...remainingNodes.splice(
-          remainingNodes.indexOf(matchingNodes.shift()),
-          1
-        )[0],
-        level: j,
-        nbChild:
-          j < treeStructure.length - 1 ? treeStructure[j + 1].numberOfNodes : 0,
-        parents: j > 0 ? lastLevel : undefined,
-        aggregatorId: uuid(),
-        finalize: j === 0
-      })
+    if (level.numberOfNodes) {
+      for (let i = 0; i < level.numberOfNodes; i++) {
+        currentLevel.push({
+          ...remainingNodes.splice(
+            remainingNodes.indexOf(matchingNodes.shift()),
+            1
+          )[0],
+          level: j,
+          nbChild:
+            j < treeStructure.length - 1
+              ? treeStructure[j + 1].numberOfNodes
+              : 0,
+          parents: j > 0 ? lastLevel : undefined,
+          aggregatorId: uuid(),
+          finalize: j === 0
+        })
+      }
+    } else {
+      while (matchingNodes.length > 0) {
+        currentLevel.push({
+          ...remainingNodes.splice(
+            remainingNodes.indexOf(matchingNodes.shift()),
+            1
+          )[0],
+          level: j,
+          nbChild:
+            j < treeStructure.length - 1
+              ? treeStructure[j + 1].numberOfNodes
+              : 0,
+          parents: j > 0 ? lastLevel : undefined,
+          aggregatorId: uuid(),
+          finalize: j === 0
+        })
+      }
+      lastLevel = currentLevel
+      break
     }
 
     lastLevel = currentLevel.slice()
-  })
+  }
 
   return lastLevel
 }
