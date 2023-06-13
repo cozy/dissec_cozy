332bc058eabe40abbd6e080a2488874849579598
JulienMirval <julien.mirval@cozycloud.cc>
2023-06-13 11:11:48 +0200

    feat: Retry directory creation

--- src/targets/services/helpers/files.js
+++ src/targets/services/helpers/files.js
@@ -2,30 +2,42 @@
  * Returns the id of the directory used to store intermediary aggregates
  * @param {CozyClient} client The client of the local instance
  */
-export const getOrCreateAppDirectory = async client => {
+export const getOrCreateAppDirectory = async (client, retries = 1) => {
   // Storing shares as files to be shared
   // Create or find a DISSEC directory
   const baseFolder = 'DISSEC'
   const parentDirectory = { _id: 'io.cozy.files.root-dir', attributes: {} }
 
-  try {
-    const { data: appDirectory } = await client
-      .collection('io.cozy.files')
-      .getDirectoryOrCreate(baseFolder, parentDirectory)
-
-    return appDirectory
-  } catch (err) {
-    if (err.status === 409) {
-      const { data } = await client
+  const getDirectory = async retriesLeft => {
+    try {
+      const { data: appDirectory } = await client
         .collection('io.cozy.files')
-        .statByPath(`/DISSEC`)
-      return data
-    } else {
-      throw new Error(
-        `Error while creating the app folder DISSEC: ${String(err)}`
-      )
+        .getDirectoryOrCreate(baseFolder, parentDirectory)
+
+      return appDirectory
+    } catch (err) {
+      const error = Array.isArray(err) ? err[0] : err
+      if (error.status === 409) {
+        const { data } = await client
+          .collection('io.cozy.files')
+          .statByPath(`/DISSEC`)
+        return data
+      } else if (
+        error.status === 500 &&
+        error.detail === 'No DB shards could be opened.' &&
+        retriesLeft > 0
+      ) {
+        console.log('CouchDB error, retrying...')
+        return await getDirectory(retriesLeft - 1)
+      } else {
+        throw new Error(
+          `Error while creating the app folder DISSEC: ${JSON.stringify(err)}`
+        )
+      }
     }
   }
+
+  return getDirectory(retries)
 }
 
 export const deleteFilesById = async (client, ids) => {
